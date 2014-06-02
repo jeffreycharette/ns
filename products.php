6800b121487b00f9d2e57236635bbe07b9b81b8c
Jeff Charette <jeffrey@wearecharette.com>
2014-06-02 13:34:03 -0400

    latest

---
+++ products.php
@@ -0,0 +1,35 @@
+<?php require 'top.php'; ?>
+<div class="white">
+	<!-- Put your top image here may need to change class above for sizing-->
+	<img src="img/natural_sea_headline.png">
+</div>
+<div class="row">
+	<div class="col-xs-4">
+		<a target="_blank" href="http://msc.org"><img src="img/bottom_left_image.png" width="300" height="380" alt="Bottom Left Image"></a>
+	</div><!-- /.col-lg-4 -->
+	<div class="col-xs-4">
+		<img src="img/bottom_middle_image.png" width="300" height="380" alt="Bottom Middle Image">
+	</div><!-- /.col-lg-4 -->
+	<div class="col-xs-4">
+		<a target="_blank" href="http://nongmoproject.org"><img src="img/bottom_right_image.png" width="300" height="380" alt="Bottom Right Image"></a>
+	</div><!-- /.col-lg-4 -->
+	<div class="col-xs-4">
+		<a target="_blank" href="http://msc.org"><img src="img/bottom_left_image.png" width="300" height="380" alt="Bottom Left Image"></a>
+	</div><!-- /.col-lg-4 -->
+	<div class="col-xs-4">
+		<img src="img/bottom_middle_image.png" width="300" height="380" alt="Bottom Middle Image">
+	</div><!-- /.col-lg-4 -->
+	<div class="col-xs-4">
+		<a target="_blank" href="http://nongmoproject.org"><img src="img/bottom_right_image.png" width="300" height="380" alt="Bottom Right Image"></a>
+	</div><!-- /.col-lg-4 -->
+	<div class="col-xs-4">
+		<a target="_blank" href="http://msc.org"><img src="img/bottom_left_image.png" width="300" height="380" alt="Bottom Left Image"></a>
+	</div><!-- /.col-lg-4 -->
+	<div class="col-xs-4">
+		<img src="img/bottom_middle_image.png" width="300" height="380" alt="Bottom Middle Image">
+	</div><!-- /.col-lg-4 -->
+	<div class="col-xs-4">
+		<a target="_blank" href="http://nongmoproject.org"><img src="img/bottom_right_image.png" width="300" height="380" alt="Bottom Right Image"></a>
+	</div><!-- /.col-lg-4 -->
+</div>
+<?php require 'bottom.php'; ?>
